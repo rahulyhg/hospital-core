1e84ebe64802dcf0f075c1b4a5c45c71640d18d0
kptpro <tung.le@robustaeng.com>
2018-09-12 03:15:41 +0000

    modified SamplePatientResource class

--- app/Http/Resource/SamplePatientResource.php
+++ app/Http/Resource/SamplePatientResource.php
@@ -18,9 +18,16 @@ class SamplePatientResource extends Resource
         return [
             'id'                => $this->id,
             'full_name'         => sprintf('%s %s', $this->first_name,$this->last_name),
+            'first_name'        => $this->first_name,
+            'last_name'         => $this->last_name,
             'birth_date'        => $this->birth_date,
             'email'             => $this->email,
-            'phone_no'             => $this->phone_no,
+            'phone_no'          => $this->phone_no,
+            'id_card_no'        => $this->id_card_no,
+            'address'           => $this->address,
+            'sex'               => $this->sex,
+            'height'            => $this->height,
+            'weight'            => $this->weight
         ];
     }
 }
