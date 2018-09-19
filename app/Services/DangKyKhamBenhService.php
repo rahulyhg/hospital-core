35e0a2c5b574a93ce42d77cacf875dafbcb46ca2
kptpro <tung.le@robustaeng.com>
2018-09-19 04:29:25 +0000

    add route DangKyKhamBenh

---
+++ app/Services/DangKyKhamBenhService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Resources\PatientResource;
+use App\Http\Resources\HosobenhanResource;
+use App\Repositories\Patient\PatientRepository;
+use Illuminate\Http\Request;
+use Validator;
+
+class DangKyKhamBenhService{
+    public function __construct()
+    {
+        
+    }
+    
+   
+}
