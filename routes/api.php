1b0ae6d831fd136b57538de3ca907280ba2d6080
kptpro <tung.le@robustaeng.com>
2018-09-19 05:11:22 +0000

    sua folder

--- routes/api.php
+++ routes/api.php
@@ -35,14 +35,15 @@ Route::group(['middleware'=>'cors', 'namespace' => 'Api\V1', 'prefix' => 'v1', '
     Route::post('patient/{id}', 'SamplePatientController@update');
     Route::delete('patient/{id}', 'SamplePatientController@delete');
     
-    Route::get('patient', 'PatientController@index');
-    //Route::post('patient/register', 'SamplePatientController@register');
-    Route::get('typepatient/{patientid}', 'HosobenhanController@typePatient');
-    //Route::get('typepatient', 'PatientController@typePatient');
+    
 
     
     Route:: group(['prefix' => 'dontiep'], function () {
-    Route::get('quetthe/{bhytcode}','DonTiep\DonTiepController@getTypePatientByCode');
+        Route::get('quetthe/{bhytcode}','DonTiep\DonTiepController@getTypePatientByCode');
+        Route::get('patient', 'DonTiep\PatientController@index');
+        //Route::post('patient/register', 'SamplePatientController@register');
+        Route::get('typepatient/{patientid}', 'DonTiep\HosobenhanController@typePatient');
+        //Route::get('typepatient', 'PatientController@typePatient');
     });
     Route:: group(['prefix' => 'dangkykhambenh'], function () {
         Route::post('dangky', 'DangKyKhamBenhController@dangky');
