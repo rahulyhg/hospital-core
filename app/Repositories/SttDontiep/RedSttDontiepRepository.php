7873acc0bf6d7f62960c66b3692c137247193afb
kptpro <tung.le@robustaeng.com>
2018-09-20 10:39:23 +0000

    add new code

--- app/Repositories/SttDontiep/RedSttDontiepRepository.php
+++ app/Repositories/SttDontiep/RedSttDontiepRepository.php
@@ -13,6 +13,20 @@ class RedSttDontiepRepository extends BaseRepositoryV2
         return RedSttDontiep::class;
     }
     
-    
-
+    public function getInfoPatientByStt($stt, $id_phong, $id_benh_vien)
+    {
+        $dieu_kien = [
+            'loai_stt'      => $stt[0],
+            'so_thu_tu'     => (int)substr($stt, 1, 4),
+            'id_phong'      => $id_phong,
+            'id_benh_vien'  => $id_benh_vien
+        ];
+        
+        $data = DB::table('red_stt_dontiep')
+                ->where($dieu_kien)
+                ->orderBy('id', 'desc')
+                ->first();
+                
+        return $data;   
+    }
 }
--- app/Services/RedSttDontiepService.php
+++ app/Services/RedSttDontiepService.php
@@ -4,34 +4,44 @@ namespace App\Services;
 
 use App\Models\RedSttDontiep;
 use App\Http\Resources\RedSttDontiepResource;
-use App\Repositories\RedSttDontiepRepository;
+use App\Repositories\SttDontiep\RedSttDontiepRepository;
 use Illuminate\Http\Request;
 use Validator;
 
 class RedSttDontiepService {
-    public function __construct(RedSttDontiepRepository $repository)
+    public function __construct(RedSttDontiepRepository $RedSttDontiepRepository)
     {
-        $this->repository = $repository;
+        $this->RedSttDontiepRepository = $RedSttDontiepRepository;
     }
     
-    public function showSttDontiep($id){
-        $stt_dontiep = $this->repository->showSttDontiep($id);
+    // public function showSttDontiep($id){
+    //     $stt_dontiep = $this->repository->showSttDontiep($id);
         
-        return new RedSttDontiepResource($stt_dontiep);
-    }
+    //     return new RedSttDontiepResource($stt_dontiep);
+    // }
     
-    public function makeSttDontiep(Request $request)
-    {
-        $this->repository->create($request->all());
-        $stt_dontiep = RedSttDontiep::orderBy('id', 'desc')->first();
+    // public function makeSttDontiep(Request $request)
+    // {
+    //     $this->repository->create($request->all());
+    //     $stt_dontiep = RedSttDontiep::orderBy('id', 'desc')->first();
         
-        return new RedSttDontiepResource($stt_dontiep);
-    }
+    //     return new RedSttDontiepResource($stt_dontiep);
+    // }
     
-    public function updateSttDontiep(Request $request, $id){
-        $stt_dontiep = $this->repository->showSttDontiep($id);
-        $this->repository->update($stt_dontiep, $request->all());
-    }
-    
+    // public function updateSttDontiep(Request $request, $id){
+    //     $stt_dontiep = $this->repository->showSttDontiep($id);
+    //     $this->repository->update($stt_dontiep, $request->all());
+    // }
    
+    public function getInfoPatientByStt($stt, $id_phong, $id_benh_vien){
+        $data = $this->RedSttDontiepRepository->getInfoPatientByStt($stt, $id_phong, $id_benh_vien);
+        
+        return new RedSttDontiepResource($data);
+    }
+    
+    public function getListPatientByKhoaPhong($loaibenhanid, $departmentid, $id_benh_vien){
+        
+        
+        //return new RedSttDontiepResource($data);
+    }
 }
