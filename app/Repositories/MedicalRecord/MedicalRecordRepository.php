db01f14e31ba893e02bca54fe91bf2a0eec5e325
kptpro <tung.le@robustaeng.com>
2018-09-21 06:54:25 +0000

    update MedicalRecordRepository

--- app/Repositories/MedicalRecord/MedicalRecordRepository.php
+++ app/Repositories/MedicalRecord/MedicalRecordRepository.php
@@ -60,7 +60,31 @@ class MedicalRecordRepository extends BaseRepositoryV2
     
     public function getListBN_PK($departmentid, $start_day, $end_day, $offset, $limit = 10)
     {
+        $loaibenhanid = 24; //kham benh
         
+        $where = [
+            ['medicalrecord.loaibenhanid', '=', $loaibenhanid],
+            ['medicalrecord.departmentid', '=', $departmentid],
+        ];
+        
+        $column = [
+            'hosobenhan.patientid',
+            'hosobenhan.patientname',
+            //'hosobenhan.birthday',
+            'hosobenhan.birthday_year',
+            'bhyt.bhytcode',
+            //'medicalrecord.thoigianvaovien'
+        ];
+        
+        $data = DB::table('medicalrecord')
+                ->join('hosobenhan', 'hosobenhan.hosobenhanid', '=', 'medicalrecord.hosobenhanid')
+                ->join('bhyt', 'bhyt.bhytid', '=', 'medicalrecord.bhytid')
+                ->where($where)
+                ->whereDate('thoigianvaovien', '=', $start_day)
+                ->orderBy('thoigianvaovien', 'asc')
+                ->offset($offset)
+                ->limit($limit)
+                ->get($column);
         
         return $data;
     }
