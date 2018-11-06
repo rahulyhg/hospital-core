9cfdd8a1273728e8522c4e4f2e662557f13b50d0
kptpro <tung.le@robustaeng.com>
2018-11-06 02:34:01 +0000

    add API getSttPhongKham

--- app/Repositories/HsbaKhoaPhong/HsbaKhoaPhongRepository.php
+++ app/Repositories/HsbaKhoaPhong/HsbaKhoaPhongRepository.php
@@ -13,17 +13,25 @@ class HsbaKhoaPhongRepository extends BaseRepositoryV2
         return HsbaKhoaPhong::class;
     }
     
-    public function getListBenhNhanHanhChanh($benhVienId, $startDay, $endDay, $limit = 20, $page = 1, $keyword = '')
+    public function getListBenhNhan($phongId, $benhVienId, $startDay, $endDay, $limit = 20, $page = 1, $keyword = '', $status = -1)
     {
         $loaiBenhAn = 24; //kham benh
         $khoaHienTai = 3; //khoa kham benh
         $offset = ($page - 1) * $limit;
         
-        $where = [
-            ['hsba_khoa_phong.loai_benh_an', '=', $loaiBenhAn],
-            ['hsba_khoa_phong.khoa_hien_tai', '=', $khoaHienTai],
-            ['hsba_khoa_phong.benh_vien_id', '=', $benhVienId]
-        ];
+        if($phongId) {  //phong kham
+            $where = [
+                ['hsba_khoa_phong.loai_benh_an', '=', $loaiBenhAn],
+                ['hsba_khoa_phong.phong_hien_tai', '=', $phongId],
+                ['hsba_khoa_phong.benh_vien_id', '=', $benhVienId]
+            ];
+        } else {    //hanh chanh don tiep
+            $where = [
+                ['hsba_khoa_phong.loai_benh_an', '=', $loaiBenhAn],
+                ['hsba_khoa_phong.khoa_hien_tai', '=', $khoaHienTai],
+                ['hsba_khoa_phong.benh_vien_id', '=', $benhVienId]
+            ];
+        }
         
         $column = [
             'hsba.id as hsba_id',
@@ -60,7 +68,7 @@ class HsbaKhoaPhongRepository extends BaseRepositoryV2
             $query = $query->whereBetween('thoi_gian_vao_vien', [Carbon::parse($startDay)->startOfDay(), Carbon::parse($endDay)->endOfDay()]);
         }
         
-        if($keyword != ''){
+        if($keyword != '') {
             $query = $query->where(function($queryAdv) use ($keyword) {
                 $upperCase = mb_convert_case($keyword, MB_CASE_UPPER, "UTF-8");
                 $lowerCase = mb_convert_case($keyword, MB_CASE_LOWER, "UTF-8");
@@ -71,97 +79,18 @@ class HsbaKhoaPhongRepository extends BaseRepositoryV2
                         ->orWhere('hsba.ten_benh_nhan', 'like', '%'.$titleCase.'%')
                         ->orWhere('hsba.ten_benh_nhan', 'like', '%'.$keyword.'%')
                         ->orWhere('hsba.ten_benh_nhan_khong_dau', 'like', '%'.$upperCase.'%')
+                        ->orWhere('hsba.ten_benh_nhan_khong_dau', 'like', '%'.$lowerCase.'%')
+                        ->orWhere('hsba.ten_benh_nhan_khong_dau', 'like', '%'.$titleCase.'%')
+                        ->orWhere('hsba.ten_benh_nhan_khong_dau', 'like', '%'.$keyword.'%')
                         ->orWhereRaw("cast(hsba.id as text) like '%$keyword%'")
                         ->orWhereRaw("cast(hsba.ms_bhyt as text) like '%$keyword%'")
                         ->orWhereRaw("cast(hsba.ms_bhyt as text) like '%$upperCase%'");
             });
         }
         
-        $totalRecord = $query->count();
-        if($totalRecord) {
-            $totalPage = ($totalRecord % $limit == 0) ? $totalRecord / $limit : ceil($totalRecord / $limit);
-            
-            $data = $query->orderBy('thoi_gian_vao_vien', 'asc')
-                        ->offset($offset)
-                        ->limit($limit)
-                        ->get($column);
-        } else {
-            $totalPage = 0;
-            $data = [];
-            $page = 0;
-            $totalRecord = 0;
-        }
-        
-        $result = [
-            'data'          => $data,
-            'page'          => $page,
-            'totalPage'     => $totalPage,
-            'totalRecord'   => $totalRecord
-        ];
-        
-        return $result;
-    }
-    
-    public function getListBenhNhanPhongKham($phongId, $benhVienId, $startDay, $endDay, $limit = 20, $page = 1, $keyword = '')
-    {
-        $loaiBenhAn = 24; //kham benh
-        $offset = ($page - 1) * $limit;
-        
-        $where = [
-            ['hsba_khoa_phong.loai_benh_an', '=', $loaiBenhAn],
-            ['hsba_khoa_phong.phong_hien_tai', '=', $phongId],
-            ['hsba_khoa_phong.benh_vien_id', '=', $benhVienId]
-        ];
-        
-        $column = [
-            'hsba.id as hsba_id',
-            'hsba.ten_benh_nhan',
-            'hsba.nam_sinh',
-            'hsba.ms_bhyt',
-            'hsba.trang_thai_hsba',
-            'hsba.ngay_tao',
-            'hsba.ngay_ra_vien',
-            'hsba_khoa_phong.thoi_gian_vao_vien',
-            'hsba_khoa_phong.thoi_gian_ra_vien',
-            'hsba_khoa_phong.id as hsba_khoa_phong_id',
-            'hsba_khoa_phong.trang_thai_cls',
-            'tt1.diengiai as ten_trang_thai_cls',
-            'hsba_khoa_phong.trang_thai',
-            'tt2.diengiai as ten_trang_thai',
-        ];
-        
-        $query = DB::table('hsba_khoa_phong')
-            ->join('hsba', 'hsba.id', '=', 'hsba_khoa_phong.hsba_id')
-            ->leftJoin('red_trangthai as tt1', function($join) {
-                $join->on('tt1.giatri', '=', 'hsba_khoa_phong.trang_thai_cls')
-                    ->where('tt1.tablename', '=', 'canlamsang');
-            })
-            ->leftJoin('red_trangthai as tt2', function($join) {
-                $join->on('tt2.giatri', '=', 'hsba_khoa_phong.trang_thai')
-                    ->where('tt2.tablename', '=', 'patientstatus');
-            })
-            ->where($where);
-        
-        if($startDay == $endDay){
-            $query = $query->whereDate('thoi_gian_vao_vien', '=', $startDay);
-        } else {
-            $query = $query->whereBetween('thoi_gian_vao_vien', [Carbon::parse($startDay)->startOfDay(), Carbon::parse($endDay)->endOfDay()]);
-        }   
-                
-        if($keyword != ''){
-            $query = $query->where(function($queryAdv) use ($keyword) {
-                $upperCase = mb_convert_case($keyword, MB_CASE_UPPER, "UTF-8");
-                $lowerCase = mb_convert_case($keyword, MB_CASE_LOWER, "UTF-8");
-                $titleCase = mb_convert_case($keyword, MB_CASE_TITLE, "UTF-8");
-                
-                $queryAdv->where('hsba.ten_benh_nhan', 'like', '%'.$upperCase.'%')
-                        ->orWhere('hsba.ten_benh_nhan', 'like', '%'.$lowerCase.'%')
-                        ->orWhere('hsba.ten_benh_nhan', 'like', '%'.$titleCase.'%')
-                        ->orWhere('hsba.ten_benh_nhan', 'like', '%'.$keyword.'%')
-                        ->orWhere('hsba.ten_benh_nhan_khong_dau', 'like', '%'.$upperCase.'%')
-                        ->orWhereRaw("cast(hsba.id as text) like '%$keyword%'")
-                        ->orWhereRaw("cast(hsba.ms_bhyt as text) like '%$keyword%'")
-                        ->orWhereRaw("cast(hsba.ms_bhyt as text) like '%$upperCase%'");
+        if($status != -1 && $phongId != 0) {
+            $query = $query->where(function($queryAdv) use ($status) {
+                $queryAdv->where('hsba_khoa_phong.trang_thai', '=', $status);
             });
         }
         
@@ -196,10 +125,10 @@ class HsbaKhoaPhongRepository extends BaseRepositoryV2
          return $id;
     }
     
-    public function updateHsbaKhoaPhong($hsbaId, $request)
+    public function updateHsbaKhoaPhong($hsbaKhoaPhongId, $params)
     {
-        $hsbaKhoaPhong = HsbaKhoaPhong::where('hsba_id', '=', $hsbaId);
-		$hsbaKhoaPhong->update($request->all());
+        $hsbaKhoaPhong = HsbaKhoaPhong::findOrFail($hsbaKhoaPhongId);
+		$hsbaKhoaPhong->update($params);
     }
 
 }
