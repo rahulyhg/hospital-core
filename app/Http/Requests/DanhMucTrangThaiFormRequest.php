b20c1d8d81ad60b6e058ce2fa32541b3f159dcf4
Duy Nguyen <nguyenvohoangduy@gmail.com>
2018-11-30 08:14:39 +0000

    fix 7

--- app/Http/Requests/DanhMucTrangThaiFormRequest.php
+++ app/Http/Requests/DanhMucTrangThaiFormRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Requests;
 
-class DanhMucDichVuFormRequest extends ApiFormRequest
+class DanhMucTrangThaiFormRequest extends ApiFormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
