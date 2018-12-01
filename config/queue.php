e42674f701f7391954dfff96c5c0baefb1c704af
Development Server <ubuntu@ip-172-31-29-97.us-west-2.compute.internal>
2018-12-01 08:06:50 +0000

    configuration for sqs,redis;add aws-sdk-php,jsonq,predis packages

--- config/queue.php
+++ config/queue.php
@@ -55,6 +55,7 @@ return [
             'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
             'queue' => env('SQS_QUEUE', 'your-queue-name'),
             'region' => env('SQS_REGION', 'us-east-1'),
+            'version' => env('SQS_VERSION','2012-11-05')
         ],
 
         'redis' => [
