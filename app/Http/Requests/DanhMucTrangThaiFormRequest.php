66ef6c817aec3dfd169bc5fdd4958f2620bc65c6
Duy Nguyen <nguyenvohoangduy@gmail.com>
2018-12-03 07:40:45 +0000

    create composite unique rule for 2 colums khoa and gia_tri of table danh_muc_trang_thai

--- app/Http/Requests/DanhMucTrangThaiFormRequest.php
+++ app/Http/Requests/DanhMucTrangThaiFormRequest.php
@@ -21,8 +21,27 @@ class DanhMucTrangThaiFormRequest extends ApiFormRequest
      */
     public function rules()
     {
+        \Validator::extend( 'composite_unique', function ( $attribute, $value, $parameters, $validator ) {
+                
+                // remove first parameter and assume it is the table name
+                $table = array_shift( $parameters ); 
+
+                // start building the conditions
+                $fields = [ $attribute => $value ]; // current field, company_code in your case
+
+                // iterates over the other parameters and build the conditions for all the required fields
+                while ( $field = array_shift( $parameters ) ) {
+                    $fields[ $field ] = $this->get( $field );
+                }
+
+                // query the table with all the conditions
+                $result = \DB::table( $table )->select( \DB::raw( 1 ) )->where( $fields )->first();
+
+                return empty( $result ); // edited here
+            }, 'your custom composite unique key validation message' );
+            
         return [
-            'khoa'      => 'required|string|unique_with:gia_tri',
+            'khoa'      => 'required|string|composite_unique:danh_muc_trang_thai,gia_tri',
             'gia_tri'   => 'required|string',
             'dien_giai' => 'required|string'
         ];
