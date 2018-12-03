53c1024ec998beb9fa0b1da7e6c8434a9125b79c
Duy Nguyen <nguyenvohoangduy@gmail.com>
2018-12-03 04:25:59 +0000

    log result from getListDMTT

--- app/Repositories/DanhMuc/DanhMucTrangThaiRepository.php
+++ app/Repositories/DanhMuc/DanhMucTrangThaiRepository.php
@@ -74,7 +74,7 @@ class DanhMucTrangThaiRepository extends BaseRepositoryV2
             'totalPage'     => $totalPage,
             'totalRecord'   => $totalRecord
         ];
-        echo("<script>console.log('PHP: ".$result."');</script>");
+        echo("<script>console.log('PHP: ".$result['page']."');</script>");
         return $data;
     }
     
