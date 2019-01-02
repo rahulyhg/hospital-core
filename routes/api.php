3ea020ab5fffd03c1c37e76d143ea92eab0f7422
Development Server <ubuntu@ip-172-31-29-97.us-west-2.compute.internal>
2019-01-02 04:13:52 +0000

    add new route dontiep/hsbaKp/cache/fromQueue

--- routes/api.php
+++ routes/api.php
@@ -57,6 +57,10 @@ Route::group(['middleware'=>'cors', 'namespace' => 'Api\V1', 'prefix' => 'v1', '
         
         Route::post('scanqrcode', 'DonTiep\ScanQRCodeController@getInfoFromCard');
         Route::post('register','DonTiep\DonTiepController@register');
+        
+        // store to cache from queue
+        Route::post('hsbaKp/cache/fromQueue','DonTiep\DonTiepController@pushToRedisFromQueue');
+        
     });
     
     Route::group(['prefix' => 'setting'], function () {
