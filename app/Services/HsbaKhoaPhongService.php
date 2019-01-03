d772c708f600ed93c5f571c90887ebc66281239b
Duy Nguyen <nguyenvohoangduy@gmail.com>
2019-01-03 10:49:32 +0000

    HM-167

--- app/Services/HsbaKhoaPhongService.php
+++ app/Services/HsbaKhoaPhongService.php
@@ -6,7 +6,10 @@ use App\Http\Resources\HsbaKhoaPhongResource;
 use App\Repositories\Hsba\HsbaKhoaPhongRepository;
 use App\Repositories\BenhVienRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Validator;
+use Storage;
+use Exception;
 
 class HsbaKhoaPhongService 
 {
@@ -25,7 +28,87 @@ class HsbaKhoaPhongService
     
     public function update($hsbaKhoaPhongId, array $params)
     {
+        $fileUpload = [];
+        // Config S3
+        $s3 = new \Aws\S3\S3Client([
+        	'region'  => config('filesystems.disks.s3.region'),
+        	'version' => 'latest',
+        	'credentials' => [
+        	    'key'    => config('filesystems.disks.s3.key'),
+        	    'secret' => config('filesystems.disks.s3.secret'),
+        	]
+        ]);
+        
+        // GET OLD FILE
+        $item = $this->hsbaKhoaPhongRepository->getById($hsbaKhoaPhongId);
+        $fileItem =  json_decode($item->upload_file_hoi_benh, true);
+        
+        
+        // Remove File old
+        if(!empty($params['oldFiles'])) {
+            foreach($fileItem as $file) {
+                if(!in_array($file, $params['oldFiles'])) {
+                    $s3->deleteObject([
+                        'Bucket' => config('filesystems.disks.s3.bucket'),
+                        'Key'    => $file
+                    ]);
+                }
+                else {
+                    $fileUpload[] = $file;
+                }
+            }
+            unset($params['oldFiles']);
+        }
+        else {
+            foreach($fileItem as $file) {
+                $s3->deleteObject([
+                    'Bucket' => config('filesystems.disks.s3.bucket'),
+                    'Key'    => $file
+                ]);
+            }
+        }
+        
+        if(!empty($params['files'])) {
+            $arrayExtension = ['jpg', 'jpeg', 'png', 'bmp', 'mp3', 'mp4', 'pdf', 'docx'];
+            foreach($params['files'] as $file) {
+                if(!in_array($file->getClientOriginalExtension(), $arrayExtension)) {
+                    throw new Exception('File chứa định dạng ko cho phép để upload');
+                }
+            }
+            
+            if (!is_dir('upload/hsbakp/hoibenh')) {
+                mkdir('upload/hsbakp/hoibenh', 0777, true);
+            }
+        
+            if(!empty($params['files'])) {
+                foreach ($params['files'] as $file) {
+                    $imageFileName = time() . '_' . rand(0, 999999) . '.' . $file->getClientOriginalExtension();
+                    //$move = $file->move('upload/hsbakp/hoibenh', $imageFileName);
+                    $fileUpload[] = $imageFileName;
+                    
+                    $result = $s3->putObject([
+                    	'Bucket' => config('filesystems.disks.s3.bucket'),
+                    	'Key'    => $imageFileName,
+                    	'SourceFile' => $file->getPathName(),
+                    	'ContentType' => $file->getMimeType()
+                    ]);
+                }
+                
+            }
+            if(!empty($fileUpload)) {
+                $params['upload_file_hoi_benh'] = json_encode($fileUpload);
+            }
+            else {
+                $params['upload_file_hoi_benh'] = NULL;
+            }
+            
+            unset($params['files']);
+        }
         $this->hsbaKhoaPhongRepository->update($hsbaKhoaPhongId, $params);
+        $data = array(
+            'status'  => 'success'
+        );
+        return $data;
     }
     
     public function getByHsbaId($hsbaId)
--- config/filesystems.php
+++ config/filesystems.php
@@ -57,11 +57,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'key' => env('S3_KEY'),
+            'secret' => env('S3_SECRET'),
+            'region' => env('S3_REGION'),
+            'bucket' => env('S3_BUCKET')
         ],
 
     ],
