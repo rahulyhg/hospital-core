03fde5db0f9e33271dd9d62dfd1f6efaa18646f7
kptpro <ubuntu@ip-172-31-29-97.us-west-2.compute.internal>
2019-01-04 04:00:58 +0000

    remove comment

--- app/Services/YLenhService.php
+++ app/Services/YLenhService.php
@@ -35,45 +35,24 @@ class YLenhService {
                 //insert table y_lenh
                 if($input['data']) {
                     foreach($input['data'] as $value) {
-                        // if($item['children']) {
-                        //     foreach($item['children'] as $value) {
-                                $array[] = [
-                                    'vien_phi_id'           => $input['vien_phi_id'],
-                                    'phieu_y_lenh_id'       => $phieuYLenhId,
-                                    'doi_tuong_benh_nhan'   => $input['doi_tuong_benh_nhan'],
-                                    'khoa_id'               => $input['khoa_id'],
-                                    'phong_id'              => $input['phong_id'],
-                                    'ma'                    => $value['ma'],
-                                    'ten'                   => $value['ten'],
-                                    'ten_bhyt'              => $value['ten_bhyt'],
-                                    'ten_nuoc_ngoai'        => $value['ten_nuoc_ngoai'],
-                                    'trang_thai'            => 0,
-                                    'gia'                   => $value['gia'],
-                                    'gia_bhyt'              => $value['gia_bhyt'],
-                                    'gia_nuoc_ngoai'        => $value['gia_nuoc_ngoai'],
-                                    'so_luong'              => $value['so_luong'],
-                                    'loai_y_lenh'           => $value['loai_nhom'],
-                                    'thoi_gian_chi_dinh'    => Carbon::now()->toDateTimeString(),
-                                ];
-                        //     }
-                        // }
-                        
-                        // $arrTemp = explode('---', $value);
-                        // $arr = explode('--', $arrTemp[1]);
-                        
-                        // $array[] = [
-                        //     'vien_phi_id'           => $input['vien_phi_id'],
-                        //     'phieu_y_lenh_id'       => $phieuYLenhId,
-                        //     'doi_tuong_benh_nhan'   => $input['doi_tuong_benh_nhan'],
-                        //     'khoa_id'               => $input['khoa_id'],
-                        //     'phong_id'              => $input['phong_id'],
-                        //     'ma'                    => $arr[0],
-                        //     'ten'                   => $arr[1],
-                        //     'trang_thai'            => 0,
-                        //     'so_luong'              => 1,
-                        //     'loai_y_lenh'           => $arrTemp[0],
-                        //     'thoi_gian_chi_dinh'    => Carbon::now()->toDateTimeString(),
-                        // ];
+                        $array[] = [
+                            'vien_phi_id'           => $input['vien_phi_id'],
+                            'phieu_y_lenh_id'       => $phieuYLenhId,
+                            'doi_tuong_benh_nhan'   => $input['doi_tuong_benh_nhan'],
+                            'khoa_id'               => $input['khoa_id'],
+                            'phong_id'              => $input['phong_id'],
+                            'ma'                    => $value['ma'],
+                            'ten'                   => $value['ten'],
+                            'ten_bhyt'              => $value['ten_bhyt'],
+                            'ten_nuoc_ngoai'        => $value['ten_nuoc_ngoai'],
+                            'trang_thai'            => 0,
+                            'gia'                   => $value['gia'],
+                            'gia_bhyt'              => $value['gia_bhyt'],
+                            'gia_nuoc_ngoai'        => $value['gia_nuoc_ngoai'],
+                            'so_luong'              => $value['so_luong'],
+                            'loai_y_lenh'           => $value['loai_nhom'],
+                            'thoi_gian_chi_dinh'    => Carbon::now()->toDateTimeString(),
+                        ];
                     }
                 }
                 $this->yLenhRepository->saveYLenh($array);
