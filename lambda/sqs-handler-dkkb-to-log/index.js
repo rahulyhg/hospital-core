42b7733f945eba48df8f2406740e4a597dde8284
Duy Nguyen <nguyenvohoangduy@gmail.com>
2019-03-14 09:24:16 +0000

    Edit file dkkb-to-log by bucket

--- lambda/sqs-handler-dkkb-to-log/index.js
+++ lambda/sqs-handler-dkkb-to-log/index.js
@@ -3,9 +3,17 @@ const S3  = new AWS.S3();
 exports.handler = (event, context, callback) => {
     event.Records.forEach( async ({ messageId, body, messageAttributes }) => {
         const messageBody = JSON.parse(body);
+        // Handle Date
+        const dateObj = new Date();
+        const month = dateObj.getUTCMonth() + 1; //months from 1-12
+        const day = dateObj.getUTCDate();
+        const year = dateObj.getUTCFullYear();
+        
+        const newdate = year + "/" + month + "/" + day;
+        const keyS3 = 'logs/dang-ky-kham-benh/user-input/' + messageAttributes.app_env.stringValue + '/' + newdate + '/' + messageBody.ho_va_ten + '.json';
         S3.putObject( {
-            Bucket: 'tien-trinh-dang-ky-benh-nhan',
-            Key: messageBody.ho_va_ten + '.json',
+            Bucket: messageAttributes.bucket.stringValue,
+            Key: keyS3,
             Body: body
         })
         .promise()
